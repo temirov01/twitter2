da483d95e96b518fae725cba94295c4c1812aaf3
Dagar <dagar90@gmail.com>
2015-03-11 15:07:02 +0000

    Add user model

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+  
+  def full_title(title)
+    base_title = "Twitter Project"
+    if title.empty?
+      base_title
+    else
+      base_title + " | " + title
+    end    
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  
+  root "static_pages#home"
+  
+  
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/signup',  to: 'users#new', via: 'get'
+  
+end
